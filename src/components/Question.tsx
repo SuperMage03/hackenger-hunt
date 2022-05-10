2ba2f68fe6aa45b676c9c3562658b6b8e864e32f
Eston Li <liyuelin2004@gmail.com>
2022-05-10 03:06:04 -0400

    Mostly Done!

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -3,18 +3,20 @@ import {Form, Button} from 'react-bootstrap';
 import '../styles/MainBox.scss';
 import axios from 'axios';
 import Swal from 'sweetalert2';
+import Cookies from 'js-cookie';
+
 
 interface prop {
-    password: string,
+    cookie: string,
     answer: string,
     question: string,
     description: string,
-    path: string,
     images?: Array<string>
 }
 
 interface submitResponse {
     correct: boolean;
+    nextToken: string;
     text: string;
     imageHint?: string;
 }
@@ -25,53 +27,64 @@ class Question extends React.Component<{}, prop> {
     constructor(props: any) {
         super(props);
         this.state = {
-            password: "",
+            cookie: "",
             answer: "",
             question: "",
             description: "",
-            path: ""
         }
     }
 
-    checkQuestion = () => {
-        axios({
+    checkQuestion = async () => {
+        const response = await axios({
             method: 'post',
-            url: `${apiUrl}submit/${this.state.path}`,
+            url: `${apiUrl}submit`,
             data: {
-                password: this.state.password,
-                answer: this.state.answer
+                answer: this.state.answer,
+                progressToken: this.state.cookie,
             }
-        }).then(response => {
-            const responseData = response.data as submitResponse;
-            Swal.fire({
+        });
+
+        const responseData = response.data as submitResponse;
+
+        try {
+            await Swal.fire({
                 icon: responseData.correct ? 'success' : 'error',
                 title: responseData.correct ? "Correct!" : 'Incorrect!',
                 text: responseData.text,
                 ...responseData.imageHint && {imageUrl: `${apiUrl}images/${responseData.imageHint}`}
             })
-        }).catch(err => {
-            Swal.fire({
+
+            if (responseData.correct) {
+                Cookies.set("progressToken", responseData.nextToken);
+                this.setState({cookie: responseData.nextToken});
+                window.location.reload();
+            }
+
+        } catch (err) {
+            await Swal.fire({
                 icon: 'error',
                 title: "Error!",
                 text: "Whoops! Something went wrong! Contact a leader for help!"
             });
-        })
+        }
     };
 
     async componentDidMount() {
         try {
-            const path = window.location.pathname.substring(1);
-            this.setState({path: path});
+            let cookieContent = Cookies.get("progressToken");
+            if (cookieContent === undefined) {
+                Cookies.set("progressToken", "");
+                cookieContent = "";
+            }
+            this.setState({cookie: cookieContent});
 
             const initial = (await axios({
-                method: 'get',
-                url: `${apiUrl}question/${path}`
+                method: 'post',
+                url: `${apiUrl}question`,
+                data: {progressToken: cookieContent}
             })).data;
             this.setState({question: initial.question, description: initial.description, images: initial.images});
 
-            if (initial.redirect) {
-                window.location.assign(initial.redirect);
-            }
         } catch (err) {
             console.log(err);
             this.setState({
@@ -91,21 +104,16 @@ class Question extends React.Component<{}, prop> {
                     <p>{this.state.description}</p>
                     {((this.state.images) && <>
                         <h5>Images:</h5>
-                        {this.state.images.map((image, i) => <img key={i} alt="question image" src={`data:image/png;base64,${image}`}/>)}
+                        {this.state.images.map((image, i) => <img key={i} alt="question image"
+                                                                  src={`data:image/png;base64,${image}`}/>)}
                     </>)}
                 </div>
-                <div className="boxBottom">
+                <div className="boxBottom mt-1">
                     <div className="form d-md-flex d-block">
-                        <Form.Control className="mx-1 flex-grow-1 inputField my-1" placeholder="Put your answer here!"
-                                      value={this.state.answer}
-                                      onChange={e => this.setState({answer: e.target.value})}>
-
-                        </Form.Control>
-
-                        <div className="d-block d-sm-flex my-1">
-                            <Form.Control className="mx-1 inputField my-1 my-sm-0" placeholder="Password!"
-                                          value={this.state.password}
-                                          onChange={e => this.setState({password: e.target.value})}>
+                        <div className="d-block flex-grow-1 d-sm-flex my-1">
+                            <Form.Control className="mx-1 inputField my-1 my-sm-0" placeholder="Answer!"
+                                          value={this.state.answer}
+                                          onChange={e => this.setState({answer: e.target.value})}>
                             </Form.Control>
                             <Button className="mx-1" onClick={this.checkQuestion}>Submit!</Button>
                         </div>
